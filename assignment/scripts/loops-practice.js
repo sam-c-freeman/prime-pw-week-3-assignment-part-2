db417b842a393873affcbe178536c226b795d2c2
Sam Freeman <samcfreeman888@gmail.com>
2022-07-26 10:48:03 -0500

    saving before trying even only loop

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,10 +13,18 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+//need to change the condition i<4 to i<6
+for(let i=0; i<6; i++){
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+//change i=0 to i=3
+for(let i=3; i<6; i++){
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
